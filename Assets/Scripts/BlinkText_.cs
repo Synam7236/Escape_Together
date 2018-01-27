9c0a828ab90cc8572fd8d164e13d25dfd5070a3a
Unknown <plf.24@orange.fr>
2018-01-27 18:07:52 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/BlinkText_.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkText_ : MonoBehaviour {
+
+    public GameObject Text_;
+
+    void Start()
+    {
+        StartCoroutine(BlinkOn());
+    }
+
+    IEnumerator BlinkOn()
+    {
+        yield return new WaitForSeconds(0.45f);
+
+        StartCoroutine(BlinkOff());
+
+        Text_.SetActive(true);
+    }
+
+    IEnumerator BlinkOff()
+    {
+        yield return new WaitForSeconds(0.45f);
+
+        StartCoroutine(BlinkOn());
+
+        Text_.SetActive(false);
+    }
+
+}
