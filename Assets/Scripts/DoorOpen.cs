18f614c825253292a8b7e5db4937dcd996110e81
Unknown <plf.24@orange.fr>
2018-01-28 10:54:23 +0100

    Git good

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+    public float degreesPerSecond;
+    public bool doorOpen = false;
+    public bool DoorIsMoving = true;
+    public bool startCouroutine = false;
+    	
+	// Update is called once per frame
+	void Update () {
+
+        if(doorOpen == true)
+        {
+            if (DoorIsMoving == true)
+            {
+                startCouroutine = true;
+
+                StartCoroutine(CountDown());
+
+                transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+            }
+        }
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(2);
+
+        DoorIsMoving = false;
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public int Minutes = 0;
+    public int Seconds = 0;
+
+    private Text m_text;
+    private float m_leftTime;
+
+    private void Awake()
+    {
+        m_text = GetComponent<Text>();
+        m_leftTime = GetInitialTime();
+    }
+
+    private void Update()
+    {
+        if (m_leftTime > 0f)
+        {
+            m_leftTime -= Time.deltaTime;
+            Minutes = GetLeftMinutes();
+            Seconds = GetLeftSeconds();
+            
+            if (m_leftTime > 0f)
+            {
+                m_text.text = "" + Minutes + ":" + Seconds.ToString("00");
+            }
+            else
+            {
+                m_text.text = "00:00";
+            }
+        }
+
+        if (Minutes == 0f && Seconds == 0f)
+        {
+            Minutes = 15;
+
+            Seconds = 00;
+        }
+    }
+
+    private float GetInitialTime()
+    {
+        return Minutes * 60f + Seconds;
+    }
+
+    private int GetLeftMinutes()
+    {
+        return Mathf.FloorToInt(m_leftTime / 60f);
+    }
+
+    private int GetLeftSeconds()
+    {
+        return Mathf.FloorToInt(m_leftTime % 60f);
+    }
+}
